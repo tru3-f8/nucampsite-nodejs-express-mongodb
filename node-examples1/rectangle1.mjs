5b1d9c76f7602c32f0eccb0000ddb438be69907b
Johnny <jliang61987@gmail.com>
2021-03-02 17:28:23 -0800

    backend

---
+++ node-examples1/rectangle1.mjs
@@ -0,0 +1,14 @@
+export const rectangle1 = (x, y) => {
+    if (x <= 0 || y <= 0) {
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`))
+    } else {
+        setTimeout(() => 
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            }),
+            2000
+        );
+    } 
+}
+
