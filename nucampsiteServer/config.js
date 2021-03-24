d30bb10ce939195db44dcd68d0532c738997368e
Johnny <jliang61987@gmail.com>
2021-03-23 18:09:46 -0700

    backend

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '842536782964438',
+        clientSecret: 'a6d9dd071f57f0881ea838b6bd73eb2a'
+    }
 }
